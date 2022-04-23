8041cc21d8e9ccb5ae59a628116de32aa0521383
ajayioyetomi <ajayi.teea@yahoo.com>
2022-04-23 23:04:15 +0100

    added update feature

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -22,4 +22,8 @@ export class TasksComponent implements OnInit {
     this.taskService.updateTaskReminder(task).subscribe()
   }
 
+  addTask(task:Task){
+    this.taskService.addTask(task).subscribe((task) => (this.tasks.push(task)))
+  }
+
 }
