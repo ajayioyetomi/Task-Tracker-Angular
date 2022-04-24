9d5e9e7f6fe07ebdf34644639a640966372f3b34
ajayioyetomi <ajayi.teea@yahoo.com>
2022-04-24 01:15:52 +0100

    added toggling the add task form

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Output , EventEmitter } from '@angular/core';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
 import { Task } from '../../Task';
 @Component({
   selector: 'app-add-task',
@@ -9,8 +11,13 @@ export class AddTaskComponent implements OnInit {
   @Output() onAddTask: EventEmitter<Task>= new EventEmitter();
   text: string = '';
   day: string = '';
-  reminder: boolean = false
-  constructor() { }
+  reminder: boolean = false;
+  showAddTask: boolean = false;
+  subscription: Subscription;
+
+  constructor(private uiService:UiService) { 
+    this.subscription = this.uiService.onToggle().subscribe(value => {this.showAddTask = value});
+  }
 
   ngOnInit(): void {
   }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -7,12 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   title: string = 'Task-Tracker';
-  constructor() { }
+  showAddTask:boolean = false;
+  subscription: Subscription;
+
+  constructor(private uiService:UiService) { 
+    this.subscription = this.uiService.onToggle().subscribe(value => {this.showAddTask = value});
+  }
 
   ngOnInit(): void {
   }
-  toggleAddTask(){
-    console.log('Add')
+  toggleAddTask():void{
+    this.uiService.toggleAddTask();
   }
 
 }
